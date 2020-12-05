e10bc284b1ddb07ad82865b64a921da5c0fd61f2
Kostya Petrov <kostyapetrov12rus@gmail.com>
2020-12-05 13:37:59 +0300

    hw Lesson7

---
+++ src/main/java/lesson_g/Test.java
@@ -0,0 +1,23 @@
+package lesson_g;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface BeforeSuite{
+
+}
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface AfterSuite{
+
+}
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Test{
+
+    int priority();
+}
